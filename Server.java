9c289131319e92105e371554ef4291ae250026f1
Brenda Solis <brendab4solis@gmail.com>
2021-07-29 22:46:28 -0700

    Will be refurbished as this file is no longer needed. Regarding server we are going to add file handling

--- Server.java
+++ Server.java
@@ -1,11 +1,25 @@
 import java.io.*;
 import java.net.*;
-
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 public class Server {
 
     static final int port = 1000;
-
+    static final static String fileName;
     public static void main(String[] args) {
+
+// setting up file handling
+    FileInputStream fis = null;
+    BufferedInputStream bis = null;
+    OutputStream os = null;
+    ServerSocket servsock = null;
+    Socket sock = null;
+
+
+
 
         // Creates a socket and bounds it to the specified port
         try (ServerSocket serverSocket = new ServerSocket(port)) {
