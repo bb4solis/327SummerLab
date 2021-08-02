d3150a45f67722b27b24a9c24ab20291c450f956
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-08-01 22:24:44 -0700

    Connect the three files

--- FileClient.java
+++ FileClient.java
@@ -4,10 +4,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.Scanner;
+import java.io.*;
 
 public class FileClient {
 
-    public final static int SOCKET_PORT = 13267; // you may change this
+    public final static int PORT = 1000; // you may change this
     public final static String SERVER = "127.0.0.1"; // localhost
     public final static String FILE_TO_RECEIVED = "./test2.txt"; // you may change this, I give a
                                                                  // different name because i don't
@@ -23,41 +25,94 @@ public class FileClient {
         int current = 0;
         FileOutputStream fos = null;
         BufferedOutputStream bos = null;
-        Socket sock = null;
+        Scanner in = new Scanner(System.in);
+        Socket socket = null;
+        // Create client socket
+        // Socket cS = new Socket("localhost", 999);
+        // to send data to the server
+        // DataOutputStream dos = new DataOutputStream(cS.getOutputStream());
+
         try {
-            sock = new Socket(SERVER, SOCKET_PORT);
-            System.out.println("Connecting...");
+            socket = new Socket(SERVER, PORT);
+            System.out.println("Connected");
 
             // receive file
             byte[] mybytearray = new byte[FILE_SIZE];
+            // byte[] mybytearray = new byte[(int) myFile.length()];
 
-            // Get input from server
-            InputStream is = sock.getInputStream();
+            // Get socket's output stream
+            // Used to send data to server
+            OutputStream output = socket.getOutputStream();
 
-            fos = new FileOutputStream(FILE_TO_RECEIVED);
-            bos = new BufferedOutputStream(fos);
-            bytesRead = is.read(mybytearray, 0, mybytearray.length);
-            current = bytesRead;
+            // Create a new PrintWriter from OutputStream
+            // Used to write formatted data to the OutputStream
+            // OutputStream writes them as a byte
+            // PrintWriter formats them to a text format
+            PrintWriter print = new PrintWriter(output, true);
 
-            do {
-                bytesRead = is.read(mybytearray, current, (mybytearray.length - current));
-                if (bytesRead >= 0)
-                    current += bytesRead;
-            } while (bytesRead > -1);
+            // Console is used to read from and write to the console
+            Console console = System.console();
+            String text = "";
 
-            bos.write(mybytearray, 0, current);
-            bos.flush();
+            // Loops until client exits
+            while (!text.equals("bye")) {
 
-            System.out.println("File " + FILE_TO_RECEIVED + " downloaded (" + current + " bytes read)");
+                // Get client inputs from console
+                text = console.readLine("Enter path name for file: ");
 
-        } finally {
-            if (fos != null)
-                fos.close();
-            if (bos != null)
-                bos.close();
-            if (sock != null)
-                sock.close();
+                // Send message to server
+                print.println(text);
+
+                // Reads data from server via client socket
+                InputStream input = socket.getInputStream();
+
+                // InputStreamReader reads bytes and decodes them into characters
+                // BufferedReader reads the characters as a string
+                BufferedReader buff = new BufferedReader(new InputStreamReader(input));
+
+                // Gets message from server and prints out a reply
+                String reply = buff.readLine();
+                System.out.println(reply);
+            }
+        } catch (IOException e) {
+            System.out.println("I/O error");
         }
+
+        // // Get input from server
+        // InputStream is = socket.getInputStream();
+
+        // fos = new FileOutputStream(FILE_TO_RECEIVED);
+        // bos = new BufferedOutputStream(fos);
+        // bytesRead = is.read(mybytearray, 0, mybytearray.length);
+        // current = bytesRead;
+
+        // do {
+        // bytesRead = is.read(mybytearray, current, (mybytearray.length - current));
+        // if (bytesRead >= 0)
+        // current += bytesRead;
+        // } while (bytesRead > -1);
+
+        // bos.write(mybytearray, 0, current);
+        // bos.flush();
+
+        // System.out.println("File " + FILE_TO_RECEIVED + " downloaded (" + current + "
+        // bytes read)");
+
+        // /*
+        // * System.out.println("Would you like to send file back?"); int ans =
+        // * in.nextInt(); if (ans != 0) {
+        // *
+        // * }
+        // */
+
+        // } finally {
+        // if (fos != null)
+        // fos.close();
+        // if (bos != null)
+        // bos.close();
+        // if (sock != null)
+        // sock.close();
+        // }
     }
 
 }
--- FileServer.java
+++ FileServer.java
@@ -1,77 +1,47 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.net.*;
 
 public class FileServer {
 
-    public final static int SOCKET_PORT = 13267; // you may change this
-    // TODO: Make an array list for storing clients, broadcast new or updated files
-    // to all clients
+    private final static int PORT = 1000; // Port Name
 
     public static void main(String[] args) throws IOException {
-        FileInputStream fis = null;
-        BufferedInputStream bis = null;
-        OutputStream os = null;
-        ServerSocket servsock = null;
-        Socket sock = null;
-        String filepath; // TODO: Ask clients for file name
-        Scanner in = new Scanner(System.in);
+        BufferedInputStream buff;
+        OutputStream output;
+        FileInputStream fileInput;
+        ServerSocket server;
+        Socket client;
+        ArrayList<ServerSocket> clients = new ArrayList<ServerSocket>();
 
-        // Ask user to input desired file Scanner temp = new Scanner(System.in);
-        System.out.print("Please enter filepath of file you would like to send: "); // TODO: Move to FileThread
-
-        filepath = in.nextLine();
+        // TODO: Change to a hash map
+        // TODO: Broadcast changes to every client
 
         try {
 
             // Makes server
-            servsock = new ServerSocket(SOCKET_PORT);
-            System.out.println("Waiting...");
+            server = new ServerSocket(PORT);
+            System.out.println("Listening on port " + PORT);
 
-            try {
+            System.out.println(server);
 
+            while (true) {
                 // Waits for a connection with a client
-                sock = servsock.accept();
-                System.out.println("Accepted connection : " + sock);
+                client = server.accept();
+                System.out.println("New Client : " + client);
 
-                // send file
-                File myFile = new File(filepath);
-                byte[] mybytearray = new byte[(int) myFile.length()];
+                System.out.println(client);
+                clients.add(server);
 
-                // A FileInputStream obtains input bytes from a file in a file system
-                fis = new FileInputStream(myFile);
+                // Create new thread with the new client
+                // .start() executes this instance and runs its run method
+                new FileThread(client).start();
 
-                // Used to buffer the input and to support the mark and reset methods
-                bis = new BufferedInputStream(fis);
-
-                // Reads bytes from this byte-input stream into the specified byte array,
-                // starting at the given offset
-                bis.read(mybytearray, 0, mybytearray.length);
-
-                // Get socket output to send data to client
-                os = sock.getOutputStream();
-                System.out.println("Sending " + filepath + "(" + mybytearray.length + " bytes)");
-
-                // Writes b.length bytes from the specified byte array to this output stream.
-                os.write(mybytearray, 0, mybytearray.length);
-
-                // Flushes this output stream and forces any buffered output bytes to be written
-                // out.
-                os.flush();
-
-                System.out.println("Done.");
-            } finally {
-                if (bis != null)
-                    bis.close();
-                if (os != null)
-                    os.close();
-                if (sock != null)
-                    sock.close();
             }
 
-        } finally {
-            if (servsock != null)
-                servsock.close();
+        } catch (IOException e) {
+            System.out.println("Server exception");
         }
     }
 }
