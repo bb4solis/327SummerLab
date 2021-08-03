051340d550db5061018b7f61a2c9d8f1a669bf27
binkang666 <54644269+binkang666@users.noreply.github.com>
2021-08-02 20:26:20 -0700

    Added Exception conditions and edit some changes

--- FileServer.java
+++ FileServer.java
@@ -6,42 +6,37 @@ import java.net.*;
 public class FileServer {
 
     private final static int PORT = 1000; // Port Name
+    private static ServerSocket server;
+    private static Socket client = null;
 
     public static void main(String[] args) throws IOException {
-        BufferedInputStream buff;
-        OutputStream output;
-        FileInputStream fileInput;
-        ServerSocket server;
-        Socket client;
-        ArrayList<ServerSocket> clients = new ArrayList<ServerSocket>();
 
         // TODO: Change to a hash map
         // TODO: Broadcast changes to every client
 
         try {
-
             // Makes server
             server = new ServerSocket(PORT);
             System.out.println("Listening on port " + PORT);
-
             System.out.println(server);
+        } catch (IOException e) {
+            System.out.println("Port already in use." + e.getMessage());
+            return;
+        }
 
-            while (true) {
+        while (true) {
+            try {
                 // Waits for a connection with a client
                 client = server.accept();
                 System.out.println("New Client : " + client);
 
-                System.out.println(client);
-                clients.add(server);
+                Thread t = new Thread(new FileThread(client));
 
-                // Create new thread with the new client
-                // .start() executes this instance and runs its run method
-                new FileThread(client).start();
+                t.start();
 
+            } catch (IOException e) {
+                System.out.println("Server exception");
             }
-
-        } catch (IOException e) {
-            System.out.println("Server exception");
         }
     }
 }
